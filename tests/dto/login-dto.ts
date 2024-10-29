a840fcc9c727e544af2379760598966045e8064d
Niamh <niamh.linguist@gmail.com>
2024-10-29 17:49:39 +0200

    updated env variable name

--- tests/dto/login-dto.ts
+++ tests/dto/login-dto.ts
@@ -1,3 +1,9 @@
+import dotenv from 'dotenv'
+import path from 'path'
+
+// Read from ".env" file.
+dotenv.config({ path: path.resolve('.env') })
+
 export class LoginDto {
   username: string
   password: string
@@ -9,7 +15,7 @@ export class LoginDto {
 
   static createLoginWithCorrectCredentials(): LoginDto {
     // eslint-disable-next-line no-undef
-    return new LoginDto(process.env.USER || '', process.env.PASSWORD || '')
+    return new LoginDto(process.env.USERNAME || '', process.env.PASSWORD || '')
   }
 
   static createLoginWithIncorrectCredentials(): LoginDto {
