dc010c1782b4fe2895ffa097b2bc9ff2a4707bb7
Niamh <niamh.linguist@gmail.com>
2024-10-30 20:33:29 +0200

    login – expanded positive case test and added additional neg case test for unallowed method

--- tests/pw/auth-flow.spec.ts
+++ tests/pw/auth-flow.spec.ts
@@ -5,6 +5,10 @@ import { LoginDto } from '../dto/login-dto'
 let baseUrl: string
 let endpoint: string
 
+function validateToken(token: string): boolean {
+  return /^eyJhb[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/.test(token)
+}
+
 test.beforeAll(async () => {
   baseUrl = 'https://backend.tallinn-learning.ee'
   endpoint = 'login/student'
@@ -18,9 +22,20 @@ test('Incorrect login credentials returns 401', async ({ request }) => {
   expect.soft(response.status()).toBe(StatusCodes.UNAUTHORIZED)
 })
 
+test('Incorrect HTTP method (get) returns 405', async ({ request }) => {
+  const response = await request.get(`${baseUrl}/${endpoint}`, {
+    data: LoginDto.createLoginWithCorrectCredentials(),
+  })
+
+  expect.soft(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED)
+})
+
 test('Correct login credentials returns 200', async ({ request }) => {
   const response = await request.post(`${baseUrl}/${endpoint}`, {
     data: LoginDto.createLoginWithCorrectCredentials(),
   })
+
   expect.soft(response.status()).toBe(StatusCodes.OK)
+  const responseText = await response.text()
+  expect.soft(validateToken(responseText)).toBe(true)
 })
